af6fc6afff8a0ba4d93793b856fb429ad6c47afb
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-22 21:18:21 +0100

    Iteration 1 - v.0.1.0

---
+++ Lab/it1/src/main/java/org/client_domain/Control_ClientLogin.java
@@ -0,0 +1,10 @@
+package org.client_domain;
+
+public class Control_ClientLogin {
+
+	public boolean verifyCredentials() {
+		// TODO - implement Control_ClientLogin.verifyCredentials
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Lab/it1_server/src/main/java/org/server_domain/Person_Controller.java
@@ -0,0 +1,14 @@
+package org.server_domain;
+
+public class Person_Controller {
+
+	/**
+	 * 
+	 * @param Person
+	 */
+	public boolean verifyCredentials(Person person) {
+		// TODO - implement Person_Controller.verifyCredentials
+		throw new UnsupportedOperationException();
+	}
+
+}
