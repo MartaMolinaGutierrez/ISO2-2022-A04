f38ab9004d65ab9bf3e61163f4f50bda0b709fea
Luis Javier Horcajada Torres <72609987+luisjaa01@users.noreply.github.com>
2021-11-24 20:53:25 +0100

    Iteration 1 - v.0.2.0
    Added the implementation for the client.

--- Lab/it1/src/main/java/org/client_domain/Control_ClientLogin.java
+++ Lab/it1/src/main/java/org/client_domain/Control_ClientLogin.java
@@ -1,6 +1,8 @@
 package org.client_domain;
 
 public class Control_ClientLogin {
+	
+	// Here is the client implementation 
 
 	public boolean verifyCredentials() {
 		// TODO - implement Control_ClientLogin.verifyCredentials
