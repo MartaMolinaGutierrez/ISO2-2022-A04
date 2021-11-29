2b53a0fa7e8a938ef34dc96bdd471e3b7d3b70f9
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-29 21:15:25 +0100

    UC2 - Implementation of addDisease - class version 0.2.0

--- Lab/it2_server/src/main/java/org/it2_server_domain/Disease_Controller.java
+++ Lab/it2_server/src/main/java/org/it2_server_domain/Disease_Controller.java
@@ -3,12 +3,21 @@ package org.it2_server_domain;
 /************************
  * 
  * @author Elena & Luis Javier
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Disease_Controller {
 	public Disease[] getPersistentDiseases() {
 		// TODO - implement Disease_Controller.getPersistentDiseases
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param disease
+	 */
+	public int addDisease(Disease disease) {
+		// TODO - implement Disease_Controller.addDisease
 		throw new UnsupportedOperationException();
 	}
 
