ab6b24000198aafb877fb9d132eec8bdc41d0172
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-29 21:14:26 +0100

    UC2 - Implementation of getPersistentDiseases() - class version 0.1.0

---
+++ Lab/it2_server/src/main/java/org/it2_server_domain/Disease_Controller.java
@@ -0,0 +1,16 @@
+package org.it2_server_domain;
+
+/************************
+ * 
+ * @author Elena & Luis Javier
+ * @version 0.1.0
+ *
+ */
+public class Disease_Controller {
+	public Disease[] getPersistentDiseases() {
+		// TODO - implement Disease_Controller.getPersistentDiseases
+		throw new UnsupportedOperationException();
+	}
+
+	
+}
