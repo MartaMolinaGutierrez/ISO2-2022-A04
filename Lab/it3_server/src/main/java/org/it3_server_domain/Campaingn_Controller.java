f4f52e9d5feeaeb49556b2088d0908887208faec
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-30 18:33:34 +0100

    UC3 - Implementation of addCampaign - class version 0.2.0

--- Lab/it3_server/src/main/java/org/it3_server_domain/Campaingn_Controller.java
+++ Lab/it3_server/src/main/java/org/it3_server_domain/Campaingn_Controller.java
@@ -2,12 +2,20 @@ package org.it3_server_domain;
 /*******
  * 
  * @author Elena & Luis Javier
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Campaingn_Controller {
 	public Campaign[] getPersistentCampaigns() {
 		// TODO - implement Campaign_Controller.getPersistentCampaigns
+		throw new UnsupportedOperationException();
+	}
+	/**
+	 * 
+	 * @param campaign
+	 */
+	public int addCampaign(Campaign campaign) {
+		// TODO - implement Campaign_Controller.addCampaign
 		throw new UnsupportedOperationException();
 	}
 }
