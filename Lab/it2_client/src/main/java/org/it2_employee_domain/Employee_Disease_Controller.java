c98ebb50be43fae2856cc56dee813e35f6fc2e93
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-30 17:17:40 +0100

    UC2 - Implemented getDiseaseList - class version 0.1.0

---
+++ Lab/it2_client/src/main/java/org/it2_employee_domain/Employee_Disease_Controller.java
@@ -0,0 +1,17 @@
+package org.it2_employee_domain;
+
+/*******
+ * 
+ * @author Elena & Luis Javier
+ * @version 0.1.0
+ *
+ */
+public class Employee_Disease_Controller {
+	private Disease_Emp[] disease_list;
+
+	public void getDiseaseList() {
+		// TODO - implement Employee_Disease_Controller.getDiseaseList
+		throw new UnsupportedOperationException();
+	}
+
+}
