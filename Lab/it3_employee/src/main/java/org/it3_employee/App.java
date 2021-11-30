3d4558969121eda6b8d8a6ee664098f0b91ba174
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-11-30 18:32:51 +0100

    UC3 - Implemented getPersistentCampaigns - class version 0.1.0

---
+++ Lab/it3_employee/src/main/java/org/it3_employee/App.java
@@ -0,0 +1,13 @@
+package org.it3_employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Lab/it3_server/src/main/java/org/it3_server_domain/Campaingn_Controller.java
@@ -0,0 +1,13 @@
+package org.it3_server_domain;
+/*******
+ * 
+ * @author Elena & Luis Javier
+ * @version 0.1.0
+ *
+ */
+public class Campaingn_Controller {
+	public Campaign[] getPersistentCampaigns() {
+		// TODO - implement Campaign_Controller.getPersistentCampaigns
+		throw new UnsupportedOperationException();
+	}
+}
