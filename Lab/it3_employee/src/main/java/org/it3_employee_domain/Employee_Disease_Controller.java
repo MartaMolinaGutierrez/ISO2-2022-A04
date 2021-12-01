27efa99eca3678f02a7d32a378afda45ce2550f0
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-12-01 08:16:14 +0100

    UC3 - Implementation of getCampaignList() - class version 0.1.0

---
+++ Lab/it3_employee/src/main/java/org/it3_employee_domain/Employee_Disease_Controller.java
@@ -0,0 +1,16 @@
+package org.it3_employee_domain;
+
+/*********
+ * 
+ * @author Elena & Luis Javier
+ * @version 0.1.0
+ *
+ */
+public class Employee_Disease_Controller {
+	private Campaign_Emp[] campaign_list;
+
+	public void getCampaignList() {
+		// TODO - implement Employee_Disease_Controller.getCampaignList
+		throw new UnsupportedOperationException();
+	}
+}
