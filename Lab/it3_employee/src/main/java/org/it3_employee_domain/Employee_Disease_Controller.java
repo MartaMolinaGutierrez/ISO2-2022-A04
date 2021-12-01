e11bd6e9c285161f3106930d42fd14df54b3e890
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-12-01 08:22:19 +0100

    UC3 - Implementation of insertCampaign() - class version 0.2.0

--- Lab/it3_employee/src/main/java/org/it3_employee_domain/Employee_Disease_Controller.java
+++ Lab/it3_employee/src/main/java/org/it3_employee_domain/Employee_Disease_Controller.java
@@ -1,9 +1,10 @@
 package org.it3_employee_domain;
 
+import org.it3_server_domain.Campaign;
 /*********
  * 
  * @author Elena & Luis Javier
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Employee_Disease_Controller {
@@ -13,4 +14,14 @@ public class Employee_Disease_Controller {
 		// TODO - implement Employee_Disease_Controller.getCampaignList
 		throw new UnsupportedOperationException();
 	}
+	
+	/**
+	 * 
+	 * @param campaign
+	 */
+	public int insertCampaign(Campaign campaign) {
+		// TODO - implement Employee_Disease_Controller.insertCampaign
+		throw new UnsupportedOperationException();
+	}
+
 }
