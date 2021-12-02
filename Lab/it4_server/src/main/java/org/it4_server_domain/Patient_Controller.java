2d10b8e3b0c497157f91397315a598d244f18125
MartaMolinaGutierrez <Marta.Molina9@alu.uclm.es>
2021-12-02 18:37:29 +0100

    UC4 - implemented Class Patient_Controller: (v0.1.0) basic functionalities

---
+++ Lab/it4_server/src/main/java/org/it4_server_domain/Patient_Controller.java
@@ -0,0 +1,20 @@
+/*******
+ * 
+ * @author Marta & Ismael
+ * @version 0.1.0
+ *
+ */
+
+package org.it4_server_domain;
+
+public class Patient_Controller {
+
+	@Override
+	public String toString() {
+		return "Patient_Controller [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+
+
+}
