f913f88c0379189e9940b43494343549497b88cc
MartaMolinaGutierrez <Marta.Molina9@alu.uclm.es>
2021-12-02 18:33:17 +0100

    UC4 - implemented Class Healthy_Quarantine: (v0.1.0) basic functionalities

---
+++ Lab/it4_server/src/main/java/org/it4_server_domain/Healthy_Quarantine.java
@@ -0,0 +1,16 @@
+/*******
+ * 
+ * @author Marta & Ismael
+ * @version 0.1.0
+ *
+ */
+package org.it4_server_domain;
+
+public class Healthy_Quarantine extends Patient {
+
+	public Healthy_Quarantine(String name, String iD, int phone, String[] state) {
+		super(name, iD, phone, state);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
