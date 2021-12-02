da870c50b695b63c39045cdbe5eca00c9484974b
MartaMolinaGutierrez <Marta.Molina9@alu.uclm.es>
2021-12-02 19:30:02 +0100

    UC4 - implemented Employee_Patient_Controller (v0.1.0):basic func

---
+++ Lab/it4_client/src/main/java/org/it4_employee_domain/Employee_Patient_Controller.java
@@ -0,0 +1,37 @@
+/*******
+ * 
+ * @author Marta and Ismael
+ * @version 0.1.0
+ *
+ */
+package org.it4_employee_domain;
+
+import java.util.Arrays;
+
+public class Employee_Patient_Controller {
+
+	private Patient_Emp[] patient_list;
+
+	public Patient_Emp[] getPatient_list() {
+		return patient_list;
+	}
+
+	public void setPatient_list(Patient_Emp[] patient_list) {
+		this.patient_list = patient_list;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee_Patient_Controller [patient_list=" + Arrays.toString(patient_list) + ", getPatient_list()="
+				+ Arrays.toString(getPatient_list()) + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+	public Employee_Patient_Controller(Patient_Emp[] patient_list) {
+		super();
+		this.patient_list = patient_list;
+	}
+
+	
+
+}
