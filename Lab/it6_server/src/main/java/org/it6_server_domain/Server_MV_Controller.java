ba08be0331aa5a5a0e97919490518f5f1af82a44
marina-prieto <Marinaprieto999@gmail.com>
2021-12-14 00:13:19 +0100

    UC6:Server_MV_Controller(v0.4.0)

--- Lab/it6_server/src/main/java/org/it6_server_domain/Server_MV_Controller.java
+++ Lab/it6_server/src/main/java/org/it6_server_domain/Server_MV_Controller.java
@@ -1,8 +1,10 @@
 package org.it6_server_domain;
+
+import org.it4_server_domain.Patient;
 /*******
  * 
  * @author Marina & Raul
- * @version 0.3.0
+ * @version 0.4.0
  *
  */
 public class Server_MV_Controller {
@@ -17,6 +19,9 @@ public class Server_MV_Controller {
 		// TODO - implement Server_MV_Controller.obtainVaccForecast
 		throw new UnsupportedOperationException();
 	}
-	
-	
+
+	public Patient[] selectPatients() {
+		// TODO - implement Server_MV_Controller.selectPatients
+		throw new UnsupportedOperationException();
+	}
 }
