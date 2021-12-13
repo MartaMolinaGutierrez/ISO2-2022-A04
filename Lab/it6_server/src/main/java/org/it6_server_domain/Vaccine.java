d0ffffe1fd85ee14ed8ac3cf9735dcb1a9e8f25e
marina-prieto <Marinaprieto999@gmail.com>
2021-12-13 23:59:43 +0100

    UC6:Vaccine(v0.1.0)

---
+++ Lab/it6_server/src/main/java/org/it6_server_domain/Vaccine.java
@@ -0,0 +1,11 @@
+package org.it6_server_domain;
+/*******
+ * 
+ * @author Marina & Raul
+ * @version 0.1.0
+ *
+ */
+public class Vaccine {
+	private String name;
+	private double forecast_estimation;
+}
