6022173ea94c04324c766a0abeff0e5a255c9f9f
topcart <Ismael.Perez3@alu.uclm.es>
2021-12-13 23:13:52 +0100

    UC5:ContactController(v0.1.0)

---
+++ Lab/it5_server/src/main/java/org/it5_server_domain/ContactController.java
@@ -0,0 +1,14 @@
+package org.it5_server_domain;
+
+import org.it4_server_domain.Patient;
+
+/*******
+ * 
+ * @author Marta Molina & Ismael Pérez
+ * @version 0.1.0
+ *
+ */
+
+public class ContactController {
+
+}
