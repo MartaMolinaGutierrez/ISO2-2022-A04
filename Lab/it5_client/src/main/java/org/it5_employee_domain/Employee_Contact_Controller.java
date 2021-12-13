94870c5a5e8bc4525ea7c1879a4eec8c7adf27b4
topcart <Ismael.Perez3@alu.uclm.es>
2021-12-13 23:29:27 +0100

    UC5:Employee_Contact_Controller(v0.1.0)

---
+++ Lab/it5_client/src/main/java/org/it5_employee_domain/Employee_Contact_Controller.java
@@ -0,0 +1,14 @@
+package org.it5_employee_domain;
+
+import org.it4_server_domain.Patient;
+
+/*******
+ * 
+ * @author Marta Molina & Ismael Pérez
+ * @version 0.1.0
+ *
+ */
+
+public class Employee_Contact_Controller {
+	private Patient patient;
+}
