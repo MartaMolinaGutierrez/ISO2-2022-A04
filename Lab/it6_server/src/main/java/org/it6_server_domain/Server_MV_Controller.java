1b65222bcfec70cd852cb7a7ca5deb15cabbd847
marina-prieto <Marinaprieto999@gmail.com>
2021-12-14 00:11:03 +0100

    UC6:Server_MV_Controller(v0.2.0)

--- Lab/it6_server/src/main/java/org/it6_server_domain/Server_MV_Controller.java
+++ Lab/it6_server/src/main/java/org/it6_server_domain/Server_MV_Controller.java
@@ -2,9 +2,16 @@ package org.it6_server_domain;
 /*******
  * 
  * @author Marina & Raul
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Server_MV_Controller {
 	private VaccCampaign vaccCampaing;
+	
+	public Schedule createScheduling() {
+		// TODO - implement Server_MV_Controller.createScheduling
+		throw new UnsupportedOperationException();
+	}
+	
+	
 }
