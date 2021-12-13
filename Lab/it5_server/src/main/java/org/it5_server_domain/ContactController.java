5fd3ce328fb0c9f5de18443016687ba57f75f784
topcart <Ismael.Perez3@alu.uclm.es>
2021-12-13 23:18:25 +0100

    UC5:ContactController(v0.2.0)

--- Lab/it5_server/src/main/java/org/it5_server_domain/ContactController.java
+++ Lab/it5_server/src/main/java/org/it5_server_domain/ContactController.java
@@ -5,10 +5,19 @@ import org.it4_server_domain.Patient;
 /*******
  * 
  * @author Marta Molina & Ismael Pérez
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 
 public class ContactController {
 
+	/**
+	 * 
+	 * @param patient
+	 */
+	public int addContact(Patient patient) {
+		// TODO - implement ContactController.addContact
+		throw new UnsupportedOperationException();
+	}
+	
 }
