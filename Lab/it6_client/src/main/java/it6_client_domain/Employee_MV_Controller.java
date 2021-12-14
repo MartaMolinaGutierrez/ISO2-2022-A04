717d11b1062eb322672d8add4bf2241bfc8ed0e5
marina-prieto <Marinaprieto999@gmail.com>
2021-12-14 02:02:16 +0100

    UC6:Schedule_Emp(v0.2.0)

--- Lab/it6_client/src/main/java/it6_client_domain/Employee_MV_Controller.java
+++ Lab/it6_client/src/main/java/it6_client_domain/Employee_MV_Controller.java
@@ -4,10 +4,15 @@ import org.it4_employee_domain.Patient_Emp;
 /*******
  * 
  * @author Marina & Raul
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Employee_MV_Controller {
 	private VaccCampaign_Emp vaccCampaign;
+	
+	public Schedule_Emp createScheduling() {
+		// TODO - implement Employee_MV_Controller.createScheduling
+		throw new UnsupportedOperationException();
+	}
 
 }
