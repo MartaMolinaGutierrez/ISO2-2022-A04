e36eeb248b697f52c5c25a05263897f5ffd0bf12
marina-prieto <Marinaprieto999@gmail.com>
2021-12-14 02:01:37 +0100

    UC6:Employee_MV_Controler(v0.1.0)

---
+++ Lab/it6_client/src/main/java/it6_client_domain/Employee_MV_Controller.java
@@ -0,0 +1,13 @@
+package it6_client_domain;
+
+import org.it4_employee_domain.Patient_Emp;
+/*******
+ * 
+ * @author Marina & Raul
+ * @version 0.1.0
+ *
+ */
+public class Employee_MV_Controller {
+	private VaccCampaign_Emp vaccCampaign;
+
+}
