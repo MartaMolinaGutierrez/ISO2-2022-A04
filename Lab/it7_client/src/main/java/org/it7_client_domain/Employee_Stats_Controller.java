2b41c427eb30dcd8da37257975bf758cd1a46be5
luisjaa01 <luisjavier.horcajada@alu.uclm.es>
2021-12-15 23:20:47 +0100

    UC7 - Employee_Stats_Controller - v0.2.0

--- Lab/it7_client/src/main/java/org/it7_client_domain/Employee_Stats_Controller.java
+++ Lab/it7_client/src/main/java/org/it7_client_domain/Employee_Stats_Controller.java
@@ -2,10 +2,15 @@ package org.it7_client_domain;
 /******
  * 
  * @author Raúl, Marina, Luis Javier & Elena
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Employee_Stats_Controller {
 	
 	private Stats statistic;
+	
+	public void selectStatistic() {
+		// TODO - implement Employee_Stats_Controller.selectStatistic
+		throw new UnsupportedOperationException();
+	}
 }
