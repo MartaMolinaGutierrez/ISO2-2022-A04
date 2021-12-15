a77b084841b981359dd44fe823f74b4acfb9e250
luisjaa01 <luisjavier.horcajada@alu.uclm.es>
2021-12-15 23:09:14 +0100

    UC7 - Server_Stats_Controller - v.0.1.0

---
+++ Lab/it7_server/src/main/java/org/it7_server_domain/Server_Stats_Controller.java
@@ -0,0 +1,16 @@
+package org.it7_server_domain;
+
+/******
+ * 
+ * @author Raúl, Marina, Luis Javier & Elena
+ * @version 0.1.0
+ *
+ */
+public class Server_Stats_Controller {
+	private Stats statistic;
+
+	
+
+	
+
+}
