c43bad4ca7714ac0cc8ce4ed7e5761bd5cfbe650
luisjaa01 <luisjavier.horcajada@alu.uclm.es>
2021-12-15 23:09:57 +0100

    UC7 - Server_Stats_Controller - v.0.2.0

--- Lab/it7_server/src/main/java/org/it7_server_domain/Server_Stats_Controller.java
+++ Lab/it7_server/src/main/java/org/it7_server_domain/Server_Stats_Controller.java
@@ -3,14 +3,16 @@ package org.it7_server_domain;
 /******
  * 
  * @author Raúl, Marina, Luis Javier & Elena
- * @version 0.1.0
+ * @version 0.2.0
  *
  */
 public class Server_Stats_Controller {
 	private Stats statistic;
 
-	
-
+	public void selectStatistic() {
+		// TODO - implement Server_Stats_Controller.selectStatistic
+		throw new UnsupportedOperationException();
+	}
 	
 
 }
